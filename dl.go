1e89deb557475ca565efec1e64fe62e3e83f2cd9
gong023 <gon.gong.gone@gmail.com>
2014-11-17 13:56:54 +0900

    initial commit

---
+++ dl.go
@@ -0,0 +1,49 @@
+package s3_bulk_dl
+
+import (
+	"io/ioutil"
+	"log"
+	"os"
+	"regexp"
+	"runtime"
+
+	"github.com/crowdmob/goamz/s3"
+)
+
+var download_count int
+
+type Downloader struct {
+	BasePath string
+	Procs    int
+	Bucket   *s3.Bucket
+	Contents []s3.Key
+}
+
+func (d *Downloader) execute() {
+	runtime.GOMAXPROCS(d.Procs)
+
+	done := make(chan bool)
+	limit := len(d.Contents)
+	for _, c := range d.Contents {
+		data, err := d.Bucket.Get(c.Key)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	<-done
+}
+
+func (d *Downloader) create_file(s3_path string, limit int, data []byte, done chan bool) {
+	full_file_path := d.BasePath + s3_path
+	full_dir_path := regexp.MustCompile(".+/").FindString(full_dir_path)
+	if err := os.MkdirAll(full_dir_path, 0766); err != nil {
+		log.Fatal(err)
+	}
+	if err := ioutil.WriteFile(full_file_path, data, 0766); err != nil {
+		log.Fatal(err)
+	}
+	download_count += 1
+	if download_count >= limit {
+		done <- true
+	}
+}
